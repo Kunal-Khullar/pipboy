8fb9155f9dd0cde4feafe40d3832ec69cbed9e11
Kunal Khullar <kunalkhullar5012@gmail.com>
2020-08-18 16:16:21 +0530

    Pipboy from Fallout 4

---
+++ js/pipboy.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var weapons = [
+        {
+         "name": "44_pistol","damage": 48, "fire-rate":6,
+         "range":119,"accuracy":66,"weight":4.2,"value":99
+        },
+        {
+         "name": "10mm_pistol","damage": 18, "fire-rate":46,
+         "range" :119,"accuracy":76,"weight":3.2,"value" :89
+        },
+        {
+         "name": "Assault","damage": 88, "fire-rate":100,
+         "range":199,"accuracy":80,"weight":14.2,"value":199
+        }
+    ];
+});
